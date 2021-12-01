c19cbd4b0016be01a0f541129f8130b10bdee636
Pham Giang <mcho942@gmail.com>
2021-12-01 16:16:16 +0700

    add Camera funtion

--- app/src/main/java/com/first_java_app/opencvtutorial/MainActivity.java
+++ app/src/main/java/com/first_java_app/opencvtutorial/MainActivity.java
@@ -7,10 +7,17 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
+import android.content.pm.ActivityInfo;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.SurfaceView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.JavaCameraView;
 import org.opencv.android.OpenCVLoader;
 import org.opencv.core.Core;
 import org.opencv.core.CvType;
@@ -27,7 +34,37 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2{
+    CameraBridgeViewBase cameraBridgeViewBase;
+    BaseLoaderCallback baseLoaderCallback;
+
+    private TextView numberOfFingersText = null;
+    int numberOfFingers = 0;
+    float p=0;
+
+    private Mat                    mRgba;
+    private Mat                    mGray;
+    private Mat                    samples;
+    private Mat                    skinRegionHSV;
+    private Mat                    blurred;
+    private Mat                    thresh;
+    private Mat                     trainData;
+    private Mat                     testData;
+
+    private Mat                     res;
+    private Mat                     hsvO;
+    private Scalar               	upper;
+    private Scalar               	lower;
+    private List<Integer>           trainLabs;
+
+    private KNearest                knn;
+    final Handler mHandler = new Handler();
+
+    final Runnable mUpdateFingerCountResults = new Runnable() {
+        public void run() {
+            updateNumberOfFingers();
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,64 +74,106 @@ public class MainActivity extends AppCompatActivity {
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
         }
-
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
         }
-
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, 1);
         }
         //----------END read and write permission----------------
         setContentView(R.layout.activity_main);
-
-        if(OpenCVLoader.initDebug()){
-            Toast.makeText(getApplicationContext(), "OpenCV loaded succesfully",Toast.LENGTH_LONG).show();
-        }else{
-            Toast.makeText(getApplicationContext(), "Couldn't load OpenCV",Toast.LENGTH_LONG).show();
-        }
-//        String imgInput = "/storage/emulated/0/Pictures/0.jpg";
-//        Scalar lower = new Scalar(0, 0.02*255, 0);  // to pick hands color range
-//        Scalar upper = new Scalar(30, 0.68*255, 255);
-//        Mat samples = new Mat();
-//        Mat skinRegionHSV = new Mat();
-//        Mat blurred = new Mat();
-//        Mat thresh = new Mat();
-//        Imgproc.cvtColor(Imgcodecs.imread(imgInput), samples, Imgproc.COLOR_BGR2HSV);
-//        Core.inRange(samples, lower, upper, skinRegionHSV);
-//        Imgproc.blur(skinRegionHSV, blurred, new org.opencv.core.Size(2, 2));
-//        Imgproc.threshold(blurred, thresh, 0, 255, Imgproc.THRESH_BINARY);
-
-//        //----------save img to phone: input: thresh---------------
-//        String filename = "thresh1.jpg";
-//        Boolean bool = null;
-//        File file = new File("/storage/emulated/0/Pictures", filename);
-//        filename = file.toString();
-//        bool = Imgcodecs.imwrite(filename, skinRegionHSV);
-//        if (bool == true)
-//            System.out.println("SUCCESS writing image to external storage");
-//        else
-//            System.out.println("Fail writing image to external storage");
-//        //----------END save img to phone---------------
+        numberOfFingersText = (TextView) findViewById(R.id.numberOfFingers);
+        this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        numberOfFingersText = (TextView) findViewById(R.id.numberOfFingers);
+        cameraBridgeViewBase = (JavaCameraView)findViewById(R.id.myCameraView);
+        cameraBridgeViewBase.setMaxFrameSize(640, 480);
+        cameraBridgeViewBase.setVisibility(SurfaceView.VISIBLE);
+        cameraBridgeViewBase.setCvCameraViewListener(this);
 
 
-        Mat trainData = new Mat(),
-                testData = new Mat();
-        List<Integer> trainLabs = new ArrayList<Integer>(),
-                testLabs = new ArrayList<Integer>();
+        baseLoaderCallback = new BaseLoaderCallback(this) {
+            @Override
+            public void onManagerConnected(int status) {
+
+                switch (status){
+                    case BaseLoaderCallback.SUCCESS:
+                        cameraBridgeViewBase.enableView();
+                        break;
+                    default:
+                        super.onManagerConnected(status);
+                        break;
+                }
+            }
+        };
+
+
+
+
+    }
+
+    @Override
+    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+        mRgba = inputFrame.rgba();
+        mGray = inputFrame.gray();
+
+
+
+        //----------SkinMask processing------------
+        Imgproc.cvtColor(mRgba, samples, Imgproc.COLOR_RGB2HSV);
+        Core.inRange(samples, lower, upper, skinRegionHSV);
+        Imgproc.blur(skinRegionHSV, blurred, new org.opencv.core.Size(2, 2));
+        Imgproc.threshold(blurred, thresh, 0, 255, Imgproc.THRESH_BINARY);
+        hsvO = thresh;
+        //----------END SkinMask processing------------
+        // we need float data for knn:
+        thresh.convertTo(thresh, CvType.CV_32F);
+        // for opencv ml, each feature has to be a single row:
+        //testData.push_back(thresh.reshape(1,1));
+
+
+        p = knn.findNearest(thresh.reshape(1,1), 1, res);
+//        System.out.println(p + " " + res.dump()); // p or res.dump() = 1 -> 1 finger
+        this.numberOfFingers = (int)p;
+        mHandler.post(mUpdateFingerCountResults);
+//        return mRgba;
+        return skinRegionHSV;
+    }
+    public void updateNumberOfFingers(){
+        numberOfFingersText.setText(String.valueOf(this.numberOfFingers));
+    }
+    @Override
+    public void onCameraViewStopped() {
+        mGray.release();
+        mRgba.release();
+    }
+
+    @Override
+    public void onCameraViewStarted(int width, int height) {
+        mGray = new Mat(width,height, CvType.CV_8UC4);
+        mRgba = new Mat(width,height, CvType.CV_8UC4);
+        samples = new Mat();
+        skinRegionHSV = new Mat();
+        blurred = new Mat();
+        thresh = new Mat();
+        res = new Mat();
+        lower = new Scalar(0, 0.02*255, 0);  // to pick hands color range
+        upper = new Scalar(30, 0.68*255, 255);
+        hsvO = new Mat();
+
+
+        trainData = new Mat();
+        trainLabs = new ArrayList<Integer>();
+        testData = new Mat();
         for(int r=0; r<2;r++){  // 5 signs  , r<2 for testing purpose, r<5 is the origin
             for(int c=0; c<40;c++){ // 40 samples per sign, sample 40 for test purpose
-                //H,S,V area
 //                Scalar lower = new Scalar(0, 0.28*255, 0);  // to pick hands color range
 //                Scalar upper = new Scalar(25, 0.68*255, 255);
-                Scalar lower = new Scalar(0, 0.02*255, 0);  // to pick hands color range
-                Scalar upper = new Scalar(30, 0.68*255, 255);
                 String imgInput = "/storage/emulated/0/Pictures/"+r+"/"+c+".jpg";
 
-                Mat samples = new Mat();
-                Mat skinRegionHSV = new Mat();
-                Mat blurred = new Mat();
-                Mat thresh = new Mat();
+//                Mat samples = new Mat();
+//                Mat skinRegionHSV = new Mat();
+//                Mat blurred = new Mat();
+//                Mat thresh = new Mat();
                 //----------SkinMask processing------------
                 Imgproc.cvtColor(Imgcodecs.imread(imgInput), samples, Imgproc.COLOR_BGR2HSV);
                 Core.inRange(samples, lower, upper, skinRegionHSV);
@@ -105,16 +184,6 @@ public class MainActivity extends AppCompatActivity {
                 //----------prepare to train using knn----------
                 // we need float data for knn:
                 thresh.convertTo(thresh, CvType.CV_32F);
-//                //-----split 50/50 to test-------
-//                if (c % 2 == 0) {
-//                    // for opencv ml, each feature has to be a single row:
-//                    trainData.push_back(thresh.reshape(1,1));
-//                    // add a label for that feature (the digit number):
-//                    trainLabs.add(r);
-//                } else {
-//                    testData.push_back(thresh.reshape(1,1));
-//                    testLabs.add(r);
-//                }
 
                 // for opencv ml, each feature has to be a single row:
                 trainData.push_back(thresh.reshape(1,1));
@@ -124,43 +193,34 @@ public class MainActivity extends AppCompatActivity {
             }
         }
         //------------train knn object----------
-        // make a Mat of the train labels, and train knn:
-        KNearest knn = KNearest.create();
+        knn = KNearest.create();   // make a Mat of the train labels, and train knn:
         knn.train(trainData, Ml.ROW_SAMPLE, Converters.vector_int_to_Mat(trainLabs));
         //------------END train knn object----------
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if(cameraBridgeViewBase!=null){
+            cameraBridgeViewBase.disableView();
+        }
+    }
 
-//        // now test predictions:
-//        for (int i=0; i<testData.rows(); i++)
-//        {
-//            Mat one_feature = testData.row(i);
-//            int testLabel = testLabs.get(i);
-//
-//            Mat res = new Mat();
-//            float p = knn.findNearest(one_feature, 1, res);
-//            System.out.println(testLabel + " " + p + " " + res.dump());
-//            //System.out: 9 9.0 [9]
-//        }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if(!OpenCVLoader.initDebug()){
+            Toast.makeText(getApplicationContext(),"there is a problem in opencv",Toast.LENGTH_SHORT).show();
+        }else{
+            baseLoaderCallback.onManagerConnected(BaseLoaderCallback.SUCCESS);
+        }
+    }
 
-//        public Mat onCameraFrame(CvCameraViewFrame inputFrame) {
-//            Scalar lower = new Scalar(0, 0.28*255, 0);  // to pick hands color range
-//            Scalar upper = new Scalar(25, 0.68*255, 255);
-//            String imgInput = inputFrame;
-//
-//            Mat samples = new Mat();
-//            Mat skinRegionHSV = new Mat();
-//            Mat blurred = new Mat();
-//            Mat thresh = new Mat();
-//            //----------SkinMask processing------------
-//            Imgproc.cvtColor(Imgcodecs.imread(imgInput), samples, Imgproc.COLOR_BGR2HSV);
-//            Core.inRange(samples, lower, upper, skinRegionHSV);
-//            Imgproc.blur(skinRegionHSV, blurred, new org.opencv.core.Size(2, 2));
-//            Imgproc.threshold(blurred, thresh, 0, 255, Imgproc.THRESH_BINARY);
-//            //----------END SkinMask processing------------
-//
-//            Mat res = new Mat();
-//            float p = knn.findNearest(thresh, 1, res);
-//            System.out.println(p + " " + res.dump()); // p or res.dump() = 1 -> 1 finger
-//        }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if(cameraBridgeViewBase!=null){
+            cameraBridgeViewBase.disableView();
+        }
     }
 }
 
